df04c8223816682b2b8ff2c7b0165f007215cbea
Ajay Mathur <ajaymathur850@gmail.com>
2024-10-22 16:01:42 +0530

    Second Commit

--- job-aggregator-ui/src/models/company.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-let CompanySchema = new mongoose.Schema({
-    name: String,
-    logo: String
-})
-
-module.exports =  mongoose.model('Companies', CompanySchema);
